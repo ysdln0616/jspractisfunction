904a67729cc4b07ba45178843ec729cc16f6558e
ysdln0616 <1419099@ed.tus.ac.jp>
2019-06-15 12:50:25 +0900

    firstpush

---
+++ function.js
@@ -0,0 +1,24 @@
+console.log("udon")
+
+
+
+
+function js_function00(){
+
+    document.querySelector("h1").innerText = "わん";
+    
+}
+
+
+
+
+
+function js_function01(){
+
+
+    let a=Number(document.querySelector("h2").innerText);
+   console.log(a+1);
+   a=a+1;
+   document.querySelector("h2").innerText =a;
+
+}
